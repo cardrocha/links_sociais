ce22f86e640e4955453fb7c6c66bf1f75a01e1df
Ricardo Rocha <cardrocharj@gmail.com>
2023-02-20 19:53:03 -0300

    commit initial

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<script setup>
+import Item from './components/item.vue'
+</script>
+
+
+<script>
+import Dados from './Dados/Services.json';
+export default {
+  data(){
+    return {
+      servicos: null
+    }
+  },
+  mounted(){
+    this.servicos = Dados;
+  }
+
+}
+
+</script>
+
+<template>
+  <header>
+    <img src="https://github.com/cardrocha.png" alt="logo vue"/>
+  <h1>Ricardo Rocha</h1>
+  </header>
+
+<hr>
+<div class="items">
+  <Item
+  v-for="servico, index in servicos"
+  :key="index"
+  :item="servico" />
+</div>
+<footer>
+  <p>Ricardo Rocha - Adaptado de <a target="_blank" href="https://www.youtube.com/@yurimarcon">Yuri Marcon</a></p>
+</footer>
+</template>
+
+<style>
+#app{
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+
+h1 {
+  text-align: center;
+}
+
+body{
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  background-image: url("https://img.freepik.com/fotos-gratis/copie-a-tecnologia-sem-fio-do-espaco-no-fundo-de-madeira_23-2148223229.jpg?w=740&t=st=1676932739~exp=1676933339~hmac=e05553320ab399930357939616e5b30d59a5ae7869d47c606140a02c9fee29a6");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  margin-bottom: 50px;
+}
+
+img {
+    width: 150px;
+    border-radius: 50%;
+    margin: auto;
+    display: block;
+    margin-top: 50px;
+}
+
+.items{
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  background-color: rgb(99, 135, 206);
+  text-align: center;
+  color: #000;
+}
+
+a {
+  list-style: none;
+  text-decoration: none;
+  color: #000;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/item.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  props: ['item']
+
+}
+
+</script>
+
+<template>
+  <div class="card">
+    <a target="_blank" :href="item.href">
+      <img :src="item.img" alt="Icone do serviços">
+    </a>
+    <span>{{ item.titulo }}</span>
+  </div>
+
+</template>
+
+<style scoped>
+div {
+  text-align: center;
+  display: block;
+  width: 100px;
+  margin: 20px 50px 20px 50px;
+}
+
+img {
+  width: 100px;
+  margin-bottom: 20px;
+}
+
+span {
+  font-weight: 900;
+}
+
+.card {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  min-width: 120px;
+  min-height: 150px;
+  border: 1px solid #f9f9f9;
+  border-radius: 5%;
+  opacity: .9;
+  box-shadow: 0 0 2em #1d1c1c;
+  transition: box-shadow .5s;
+}
+
+.card a {
+  margin-top: -45px;
+}
+
+.card:hover {
+  box-shadow: 0 0 4em #1d1c1c;
+  opacity: 1;
+}
+</style>
